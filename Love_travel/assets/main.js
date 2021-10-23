86c3ab5098d361e55eac3a7c2600b948e185ddf9
phamcuong05 <74145762+phamcuong05@users.noreply.github.com>
2021-10-23 21:41:51 +0700

    lovetravel

---
+++ Love_travel/assets/main.js
@@ -0,0 +1,14 @@
+// Menu navbar
+// const menuBtn = document.querySelector('.js-menu-btn')
+// const menu = document.querySelector('.js-navbar-menu')
+// const menuclose = document.querySelector('.js-menu-close')
+
+// function showMenu() {
+//     menu.classList.add('open')
+// }
+// menuBtn.addEventListener('click', showMenu)
+
+// function hideMenu() {
+//     menu.classList.remove('open')
+// }
+// menuclose.addEventListener('click', hideMenu)
